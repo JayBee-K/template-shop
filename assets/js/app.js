47208d8f53b127a7ad2ee9921d9b1a8bf3d8b319
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2023-02-07 16:09:47 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,29 @@
 	'use strict';
 	let windowWidth = $(window).width();
 
+	const handleSliderHero = function () {
+		new Swiper('#slider-hero .swiper', {
+			speed: 1500,
+			slidesPerView: 1,
+			preloadImages: false,
+			effect: 'slide',
+			loop: true,
+			autoplay: {
+				delay: 8000,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: "#slider-hero .hero-pagination",
+				clickable: true,
+			},
+		});
+	}
+
 
 	$(function () {
+		handleSliderHero();
+
+
 		$(window).resize(() => {
 			windowWidth = $(window).width();
 		});
