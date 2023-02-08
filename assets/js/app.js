c30341daaf55dfd022cedee39daeb8ba0c52dc33
Jay <killerdrug007@gmail.com>
2023-02-08 21:15:46 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,27 +3,58 @@
 	let windowWidth = $(window).width();
 
 	const handleSliderHero = function () {
-		new Swiper('#slider-hero .swiper', {
-			speed: 1500,
-			slidesPerView: 1,
-			preloadImages: false,
-			effect: 'slide',
-			loop: true,
-			autoplay: {
-				delay: 8000,
-				disableOnInteraction: false,
-			},
-			pagination: {
-				el: "#slider-hero .hero-pagination",
-				clickable: true,
-			},
-		});
+		if ($('#slider-hero').length) {
+			new Swiper('#slider-hero .swiper', {
+				speed: 1500,
+				slidesPerView: 1,
+				preloadImages: false,
+				effect: 'slide',
+				loop: true,
+				autoplay: {
+					delay: 8000,
+					disableOnInteraction: false,
+				},
+				pagination: {
+					el: "#slider-hero .hero-pagination",
+					clickable: true,
+				},
+			});
+		}
+	}
+
+	const handleSliderFeedback = function () {
+		if ($('#slider-feedback').length) {
+			new Swiper('#slider-feedback .swiper', {
+				slidesPerView: 1.5,
+				spaceBetween: 30,
+				speeds: 750,
+				loop: true,
+				autoplay: {
+					delay: 6000,
+				},
+				navigation: {
+					nextEl: '#slider-feedback .button-next',
+					prevEl: '#slider-feedback .button-prev',
+				},
+				breakpoints: {
+					375: {
+						slidesPerView: 2.5
+					},
+					992: {
+						slidesPerView: 3.5
+					},
+					1200: {
+						slidesPerView: 4
+					}
+				}
+			});
+		}
 	}
 
 
 	$(function () {
 		handleSliderHero();
-
+		handleSliderFeedback();
 
 		$(window).resize(() => {
 			windowWidth = $(window).width();
